69905140f447d8e9c9b84f5aa329fd1d4b488a62
Harken1991 <perryethan19@gmail.com>
2025-06-04 12:03:18 -0400

    FPS lock and Asteroid sprites added

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import pygame
 from constants import *
+
+# setup
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 black = (0, 0, 0)
+clock = pygame.time.Clock()
+dt = 0
 
 
 def starting_asteroids():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
+
+        dt = clock.tick(60) / 1000
+        print(f"Delta time (seconds): {dt}")
 
 
 screen.fill(black)
 pygame.display.flip()
 
-
+# start game loop
 starting_asteroids()
 
 if __name__ == "__main__":
