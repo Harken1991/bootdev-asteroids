9ef614471461d3f52ad85790b464c84cc98247f7
Harken1991 <perryethan19@gmail.com>
2025-06-05 13:37:45 -0400

    Moving triangle and grouping

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-import pygame
+import pygame  # type:ignore
 from constants import *
+from player import Player
 
 # setup
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-black = (0, 0, 0)
-clock = pygame.time.Clock()
-dt = 0
 
 
-def starting_asteroids():
+def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    running = True
-    while running:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
         dt = clock.tick(60) / 1000
-        print(f"Delta time (seconds): {dt}")
 
-
-screen.fill(black)
-pygame.display.flip()
-
-# start game loop
-starting_asteroids()
 
 if __name__ == "__main__":
     main()
